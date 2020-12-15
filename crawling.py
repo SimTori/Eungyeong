a1c598fbb25bf351bc17469ecb63f7277c5bc114
SimTori <cjh6574@gmail.com>
2020-12-15 12:29:43 +0900

    다후타 추가 (command="/다후타채권")

--- crawling.py
+++ crawling.py
@@ -3,6 +3,7 @@ import requests
 import pprint
 # 옷감 0 / 가죽 5 / 목재 10 / 철 주괴 15
 
+
 def crwaling():
     preurl = requests.get('https://archeage.xlgames.com/play/worldinfo')
     soup = BeautifulSoup(preurl.text, 'html.parser')
@@ -47,4 +48,45 @@ def crwaling():
     return spl
 
 
-crwaling()
+def crwaling_daguta():
+    preurl = requests.get('https://archeage.xlgames.com/play/worldinfo/DAHUTA')
+    soup = BeautifulSoup(preurl.text, 'html.parser')
+    text = ['옷감', '가죽', '목재', '철 주괴']
+    data = soup.select('tbody')
+    arr1 = []
+    test = []
+    spl = []
+
+    for i in range(0, 2):
+        arr1.append(data[i].text)
+
+    for data in arr1:
+        test.append(data.split('\n'))
+
+    for ta in test:
+        spl.append(list(filter(None, ta)))
+
+
+    for sp in spl:
+        for i in range(0, 4):
+            if sp.index(text[1]) != 5:
+                sp.insert(sp.index(text[1]), '　　　　　　　　　')
+            else:
+                break
+        for i in range(0, 4):
+            if sp.index(text[2]) != 10:
+                sp.insert(sp.index(text[2]), '　　　　　　　　　')
+            else:
+                break
+        for i in range(0, 4):
+            if sp.index(text[3]) != 15:
+                sp.insert(sp.index(text[2]), '　　　　　　　　　')
+            else:
+                break
+        for i in range(0, 4):
+            if len(sp) != 20:
+                sp.insert(len(sp), '-------------------------')
+            else:
+                break
+
+    return spl
--- main.py
+++ main.py
@@ -39,5 +39,18 @@ async def display(ctx):
 
     await ctx.send(embed=embed)
 
+
+@clinet.command(name='다후타채권')
+async def display(ctx):
+    data = crawling.crwaling()
+    embed = di.Embed(colour=di.Colour.dark_gold())
+    embed.set_author(name="채권")
+    embed.add_field(name='옷감', value=insertL(data, 1, 5), inline=False)
+    embed.add_field(name='가죽', value=insertL(data, 6, 10), inline=False)
+    embed.add_field(name='목재', value=insertL(data, 11, 15), inline=False)
+    embed.add_field(name='철 주괴', value=insertL(data, 16, 20), inline=False)
+
+    await ctx.send(embed=embed)
+
 clinet.run(os.environ['token'])
 
