2031eef3005ee38f13019ffdac1335e96035bf40
SimTori <cjh6574@gmail.com>
2020-12-15 12:31:47 +0900

    다후타 추가 (command="/다후타채권")

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def on_ready():
 
 @clinet.command(name='채권')
 async def display(ctx):
-    data = crawling.crwaling()
+    data = crawling.crwaling_daguta()
     embed = di.Embed(colour=di.Colour.dark_gold())
     embed.set_author(name="채권")
     embed.add_field(name='옷감', value=insertL(data, 1, 5), inline=False)
