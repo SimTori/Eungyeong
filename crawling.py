9fb4ab52b0f3f14637855a14566a204a351c131b
SimTori <cjh6574@gmail.com>
2021-09-29 15:14:36 +0900

    v1.2

--- crawling.py
+++ crawling.py
@@ -1,11 +1,19 @@
-from bs4 import BeautifulSoup
 import requests
-import pprint
+from bs4 import BeautifulSoup
+
 # 옷감 0 / 가죽 5 / 목재 10 / 철 주괴 15
+SERVER = {
+    "다후타": "DAHUTA",
+    "누이": "NUI",
+    "하제": "HAJE",
+    "모르페우스": "MORPHEUS",
+    "랑그레이": "RANGORA",
+    "환락": "SEASON",
+}
 
 
-def crwaling():
-    preurl = requests.get('https://archeage.xlgames.com/play/worldinfo')
+def crwaling(server: str):
+    preurl = requests.get('https://archeage.xlgames.com/play/worldinfo/{}'.format(SERVER[server]))
     soup = BeautifulSoup(preurl.text, 'html.parser')
     text = ['옷감', '가죽', '목재', '철 주괴']
     data = soup.select('tbody')
@@ -21,51 +29,6 @@ def crwaling():
 
     for ta in test:
         spl.append(list(filter(None, ta)))
-
-
-    for sp in spl:
-        for i in range(0, 4):
-            if sp.index(text[1]) != 5:
-                sp.insert(sp.index(text[1]), '　　　　　　　　　')
-            else:
-                break
-        for i in range(0, 4):
-            if sp.index(text[2]) != 10:
-                sp.insert(sp.index(text[2]), '　　　　　　　　　')
-            else:
-                break
-        for i in range(0, 4):
-            if sp.index(text[3]) != 15:
-                sp.insert(sp.index(text[2]), '　　　　　　　　　')
-            else:
-                break
-        for i in range(0, 4):
-            if len(sp) != 20:
-                sp.insert(len(sp), '-------------------------')
-            else:
-                break
-
-    return spl
-
-
-def crwaling_daguta():
-    preurl = requests.get('https://archeage.xlgames.com/play/worldinfo/DAHUTA')
-    soup = BeautifulSoup(preurl.text, 'html.parser')
-    text = ['옷감', '가죽', '목재', '철 주괴']
-    data = soup.select('tbody')
-    arr1 = []
-    test = []
-    spl = []
-
-    for i in range(0, 2):
-        arr1.append(data[i].text)
-
-    for data in arr1:
-        test.append(data.split('\n'))
-
-    for ta in test:
-        spl.append(list(filter(None, ta)))
-
 
     for sp in spl:
         for i in range(0, 4):
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from discord.ext import commands
-import discord as di
-import crawling
 import os
 
-clinet = commands.Bot(command_prefix="/")
+import discord as di
+from discord.ext import commands
+
+import crawling
+
+bot = commands.Bot(command_prefix="!")
+
 
 def insertL(data, j, k):
-    st = ''
+    result = ''
     for i in range(j, k):
         space = ''
         if len(data[0][i]) < 13:
@@ -17,42 +20,41 @@ def insertL(data, j, k):
         else:
             space = space + '　　'
 
-        st = st + str(data[0][i]) + space + str(data[1][i]) + '\n'
-    return st
+        result = result + str(data[0][i]) + space + str(data[1][i]) + '\n'
+    return result
 
-@clinet.event
+
+@bot.event
 async def on_ready():
     print("Logged in as")
-    print(clinet.user.name)
-    print(clinet.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('--------------')
 
-@clinet.command(name='채권')
-async def display(ctx):
-    data = crawling.crwaling()
+
+@bot.command(name='채권')
+async def display(ctx, server: str):
+    data = crawling.crwaling(server)
     embed = di.Embed(colour=di.Colour.dark_gold())
     embed.set_author(name="채권")
     embed.add_field(name='옷감', value=insertL(data, 1, 5), inline=False)
     embed.add_field(name='가죽', value=insertL(data, 6, 10), inline=False)
     embed.add_field(name='목재', value=insertL(data, 11, 15), inline=False)
     embed.add_field(name='철 주괴', value=insertL(data, 16, 20), inline=False)
-
     await ctx.send(embed=embed)
 
 
-@clinet.command(name='다후타채권')
-async def display(ctx):
-    data = crawling.crwaling_daguta()
+@display.error
+async def display_error(ctx, error):
+    error_msg = "!채권 서버명\n"
+    error_msg += "ex) !채권 하제"
+
     embed = di.Embed(colour=di.Colour.dark_gold())
-    embed.set_author(name="채권")
-    embed.add_field(name='옷감', value=insertL(data, 1, 5), inline=False)
-    embed.add_field(name='가죽', value=insertL(data, 6, 10), inline=False)
-    embed.add_field(name='목재', value=insertL(data, 11, 15), inline=False)
-    embed.add_field(name='철 주괴', value=insertL(data, 16, 20), inline=False)
+    embed.set_author(name="채권 명령어")
+    embed.add_field(name="명령어", value=error_msg, inline=False)
 
+    embed.add_field(name="서버 목록", value="누이, 하제, 다후타, 모르페우스, 환락", inline=False)
     await ctx.send(embed=embed)
 
-clinet.run(os.environ['token'])
 
-def test():
-    return
+bot.run(os.environ['token'])
